e564d2257ada807293b4409ccdc6268af47d9c1b
laherasif <47274948+laherasif@users.noreply.github.com>
2024-07-21 13:31:55 +0500

    resolve last link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <h1 className='text-[40px] font-bold'>Test Assignments</h1>
         <Link to="/checkout" className='text-[30px] pt-5'>Task No 1 and Task 2  : Click Here (checkout Page With Sidebar ) </Link>
         <Link to="/products" className='text-[30px] pt-5'>Task No 3 : Click Here (Sidebar With Filter) </Link>
-        <Link to="/geCommerce" target='_blank' className='text-[30px] pt-5'>Task No 4 and Task No 4.1 : Click Here (GeCommerce web and mobile view with  Rightbar ) </Link>
+        <Link to="/geCommerce" className='text-[30px] pt-5'>Task No 4 and Task No 4.1 : Click Here (GeCommerce web and mobile view with  Rightbar ) </Link>
       </div>
     </div>
   )
